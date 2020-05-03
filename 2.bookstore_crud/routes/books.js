a15142f517957f2130a25c9e72761eb11fffff1d
anshusaurav <anshu.saurav@gmail.com>
2020-05-03 17:54:38 +0530

    Check

--- 2.bookstore_crud/routes/books.js
+++ 2.bookstore_crud/routes/books.js
@@ -6,33 +6,40 @@ var router = express.Router();      //extract router module from express
 //All books page
 
 router.get("/", (req, res, next) =>{
-    console.log('All here');
     
     Book.find({}, (err, listBooks) =>{
         if(err)
             return next(err);
-        console.log(listBooks);
         return res.render("allBook",{books: listBooks });
     });
 });
 
-//Add book page
+//Add book
 router.get("/new", (req, res, next) =>{
-    console.log('Addbook');
     res.render("newBook")
 });
 
 
 router.post("/new", (req, res, next) =>{
-    console.log('POSTBOOK')
     Book.create(req.body, (err, data) => {
         if(err) return next(err);
-        console.log(req.body);
         return res.redirect('/books');
     });
 });
 
 
-//view books
+//view Single book
+
+router.get("/:id", (req, res, next) =>{
+    var _id = req.params.id;
+    console.log('ID', id);
+    Book.findById(_id, (err, book) =>{
+        console.log(book);
+        if(err)
+            return next(err);
+        console.log(book);
+        return res.render('viewBook', {book})
+    });
+});
 
 module.exports = router;
